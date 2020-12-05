29f4cb40063149e5487e4d5d69b9a1760c002c18
Sebastian Pratt <sebastianpratt98@gmail.com>
2020-12-05 12:22:33 -0300

    Creacion de la ventana editorial

---
+++ WpfBibliotecaComunitaria/Editoriales.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfBibliotecaComunitaria
+{
+    /// <summary>
+    /// Lógica de interacción para Editoriales.xaml
+    /// </summary>
+    public partial class Editoriales : Window
+    {
+        public Editoriales()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win1 = new MainWindow();
+            win1.Show();
+            this.Close();
+        }
+    }
+}
